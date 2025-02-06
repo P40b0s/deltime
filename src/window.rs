a55fb9d89a1974a0bae176f6970c3c621b7e34cb
P40b0s <uranius666@gmail.com>
2025-02-06 13:50:01 +0300

    remove not used dependences

--- src/window.rs
+++ src/window.rs
@@ -1,40 +1,8 @@
-//use fltk::{app, enums::Color, prelude::*, window::Window};
-
-
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
 use winit::window::{Window, WindowId};
 
-// fn alpha_composited_color(col: Color, bg_col: Color, alpha: f32) -> Color 
-// {
-//     let tup = col.to_rgb();
-//     let bg_col = bg_col.to_rgb();
-//     let r = alpha * tup.0 as f32 + (1.0 - alpha) * bg_col.0 as f32;
-//     let r = r as u8;
-//     let g = alpha * tup.1 as f32 + (1.0 - alpha) * bg_col.1 as f32;
-//     let g = g as u8;
-//     let b = alpha * tup.2 as f32 + (1.0 - alpha) * bg_col.2 as f32;
-//     let b = b as u8;
-//     Color::from_rgb(r, g, b)
-
-// }
-// pub fn start2() 
-// {
-//     let app = app::App::default();
-    
-//     let mut wind = Window::new(0, 0, 100, 100, "Hello from rust");
-//     wind.set_color(Color::Free);
-//     wind.end();
-//     wind.show();
-//     wind.set_opacity(0.0);
-    
-//     while app.wait()
-//     {
-
-//     }
-//     //app.run().unwrap();
-// }
 #[derive(Default)]
 struct App 
 {
